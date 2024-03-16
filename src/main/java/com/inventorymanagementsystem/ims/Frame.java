37376844bfcc75ba32f5b1c76a0ca1f62ba4b919
Hilary <58515831+Diaz123-droid@users.noreply.github.com>
2024-03-16 23:43:43 +0200

    Changed left pane design.

--- src/main/java/com/inventorymanagementsystem/ims/Frame.java
+++ src/main/java/com/inventorymanagementsystem/ims/Frame.java
@@ -2,6 +2,7 @@ package com.inventorymanagementsystem.ims;
 
 import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
+
 import java.awt.event.*;
 import java.text.MessageFormat;
 import java.awt.*;
@@ -24,9 +25,15 @@ public class Frame extends JFrame implements ActionListener {
 
         // Create left pane wih a tree
         JTree tree = new JTree(LeftPaneTreeView());
+        tree.setEditable(false);
+        tree.setScrollsOnExpand(true);
+        tree.setEnabled(true);
+        tree.setExpandsSelectedPaths(true);
+        tree.setAutoscrolls(true);
+        tree.setFont(new Font("Dialogs", Font.PLAIN, 12));
+        tree.setRootVisible(false);
         JScrollPane treeScrollPane = new JScrollPane(tree);
-        tree.setBackground(new Color(39, 52, 68));
-        treeScrollPane.setPreferredSize(new Dimension(200, getHeight())); // Set fixed width for tree
+        treeScrollPane.setPreferredSize(new Dimension(250, getHeight())); // Set fixed width for tree
 
         // Create center view with a book view (placeholder)
         JPanel centerView = new JPanel();
@@ -62,21 +69,24 @@ public class Frame extends JFrame implements ActionListener {
         this.setVisible(true); // display frame
         ImageIcon icon = new ImageIcon("src\\main\\resources\\icons\\imslogocolored.png");
         this.setIconImage(icon.getImage()); // Set image as window icon
-        this.getContentPane().setBackground(new Color(5, 24, 33)); // bg color for frame
         this.setLocationRelativeTo(null);
     }
 
+    /**
+     * This method returns a selectable tree view to parent frame
+     * @return DefaultMutableTreeNode
+     */
     public DefaultMutableTreeNode LeftPaneTreeView() {
-        final String nodeTitles[] = { "inventory", "supplier", "Product", "Sales", "Purchase", "Planner", "Settings" };
+        final String nodeTitles[] = { "Inventory", "supplier", "Product", "Sales", "Purchase", "Planner", "Settings" };
 
         // Create root node / root nodes
-        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("[IMS ROOT]");
+        final DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("[IMS ROOT]");
 
         for (int index = 0; index < 7; index++) {
             DefaultMutableTreeNode node = new DefaultMutableTreeNode(String.format("[%s]", nodeTitles[index]));
 
             for (int i = 0; i < (int) (Math.random() * 10); i++) {
-                node.add(new DefaultMutableTreeNode(MessageFormat.format("{0} Child {1}", nodeTitles[index], i)));
+                node.add(new DefaultMutableTreeNode(MessageFormat.format("{0} Leaf Node {1}", nodeTitles[index], i)));
             }
 
             rootNode.add(node);
@@ -85,6 +95,10 @@ public class Frame extends JFrame implements ActionListener {
         return rootNode; // Return root node to Calling Frame
     }
 
+    /**
+     * This mathod returns a bookstyle menu bar to parent frame
+     * @return JTabbedPane
+     */
     public JTabbedPane TabMenu() {
         // Tabbed View menu bar
         JTabbedPane tabMenuBar = new JTabbedPane();
@@ -94,23 +108,25 @@ public class Frame extends JFrame implements ActionListener {
         String titles[] = {
                 "Products", "Inventory", "Sales", "Reports", "Settings", "Help", "Form", "Views"
         };
-        @SuppressWarnings("unused")
-        int mnemonic[] = {
-                KeyEvent.VK_P, KeyEvent.VK_I, KeyEvent.VK_S, KeyEvent.VK_R, KeyEvent.VK_T, KeyEvent.VK_H,
-                KeyEvent.VK_F, KeyEvent.VK_V
-        };
+
         JPanel panels[] = {
                 new JPanel(), new JPanel(), new JPanel(), new JPanel(), new JPanel(), new JPanel(), new JPanel(),
                 new JPanel()
         };
 
         for (int i = 0, n = titles.length; i < n; i++) {
-            panels[i].setBackground(new Color(39, 52, 68));
             panels[i].add(new JLabel(titles[i], SwingConstants.CENTER)); // Set label for each panel
             tabMenuBar.addTab(titles[i], null, panels[i], MessageFormat.format("{0} Tab Description", titles[i]));
         }
 
         return tabMenuBar;
+    }
+
+    public JPanel productsPanel(){
+        JPanel productsPanelObject = new JPanel();
+        // BorderLayout layout = new BorderLayout(0,0);
+        
+        return productsPanelObject;
     }
 
     @Override
